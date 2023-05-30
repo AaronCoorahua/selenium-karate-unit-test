de2a6e08b818542893f3bd8cfe80fcb48be31a08
MaxAntunez404 <max.antunez@utec.edu.pe>
2023-05-29 19:56:23 -0500

    fizzbuzz_test

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,11 @@
-for fizzbuzz in range(51):
-    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
-        print("fizzbuzz")
-        continue
-    elif fizzbuzz % 3 == 0:
-        print("fizz")
-        continue
-    elif fizzbuzz % 5 == 0:
-        print("buzz")
-        continue
-    print(fizzbuzz)
+
+def fizzbuzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "fizzbuzz"
+    elif num % 3 == 0:
+        return "fizz"
+    elif num % 5 == 0:
+        return "buzz"
+    else:
+        return str(num)
+
---
+++ fizzbuzz_tests.py
@@ -0,0 +1,26 @@
+import unittest
+
+class FizzBuzzTests(unittest.TestCase):
+
+    def test_fizz(self):
+        self.assertEqual(fizzbuzz(3), "fizz")
+        self.assertEqual(fizzbuzz(6), "fizz")
+        self.assertEqual(fizzbuzz(9), "fizz")
+
+    def test_buzz(self):
+        self.assertEqual(fizzbuzz(5), "buzz")
+        self.assertEqual(fizzbuzz(10), "buzz")
+        self.assertEqual(fizzbuzz(20), "buzz")
+
+    def test_fizzbuzz(self):
+        self.assertEqual(fizzbuzz(15), "fizzbuzz")
+        self.assertEqual(fizzbuzz(30), "fizzbuzz")
+        self.assertEqual(fizzbuzz(45), "fizzbuzz")
+
+    def test_other_numbers(self):
+        self.assertEqual(fizzbuzz(1), "1")
+        self.assertEqual(fizzbuzz(2), "2")
+        self.assertEqual(fizzbuzz(7), "7")
+
+if __name__ == '__main__':
+    unittest.main()

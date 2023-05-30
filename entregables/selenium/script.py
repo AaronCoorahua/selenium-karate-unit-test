8a741b9fa4154c05fb1c6cee2f56a4786fedf2da
Aaron Coorahua <ruben.coorahua@utec.edu.pe>
2023-05-29 21:01:26 -0500

    actulizacion entregables

--- entregables/selenium/script.py
+++ entregables/selenium/script.py
@@ -3,14 +3,19 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-#driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 driver.get("http://localhost:3000")
 
 # TESTEAR ingresar un valor a una caja de texto del miembro que le corresponda. 
 
-#driver.save_screenshot("screenshot.png")
+title = driver.title
+driver.implicitly_wait(0.5)
+text_box = driver.find_element(by=By.NAME, value="my-text")
+submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+text_box.send_keys("Selenium")
+submit_button.click()
 
-#driver.quit()
+driver.save_screenshot("screenshot.png")
 
-# python3 -m pip install urllib3==1.26.6
+driver.quit()
